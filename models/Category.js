ab3ea3a3d3703c923bb19c700583e54055e137f4
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-19 10:31:25 +0100

    DATA: Updated category model

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,18 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+
   },
   {
     sequelize,
