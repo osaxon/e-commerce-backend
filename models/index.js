f42fb524ef1486a2fc93f2314978cf9477746b2c
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-19 11:05:14 +0100

    FEAT: Get all categories API route working

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product',
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+})
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+})
 
 module.exports = {
   Product,
