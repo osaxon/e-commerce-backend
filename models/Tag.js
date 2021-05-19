38742e629878def75f8170640278ce9b99be440a
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-19 10:46:55 +0100

    DATA: Tag model updated

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,18 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+
   },
   {
     sequelize,
