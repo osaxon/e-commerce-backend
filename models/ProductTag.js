506e1355fced79766abd09267702f199ddb15f17
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-19 10:43:50 +0100

    DATA: ProductTag model updated

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,23 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+
+    product_id: {
+      type: DataTypes.INTEGER,
+      model: 'product',
+    },
+
+    tag_id: {
+      type: DataTypes.INTEGER,
+      model: 'tag',
+    }
+
   },
   {
     sequelize,
