8406ebf60caccb91febbdaf014f4ea3be5f3f6c2
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-21 18:54:33 +0100

    FEAT: Tag delete route added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -55,6 +55,15 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  const tagID = req.params.id;
+  try {
+    const tagData = await Tag.destroy({
+      where: { id: tagID }
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
