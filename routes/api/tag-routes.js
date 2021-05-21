4c37ac04e35e583c9620e2217170d98f5b630ed6
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-21 18:29:24 +0100

    FEAT: Add new Tag route working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -17,13 +17,28 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag }]
+    });
+      res.status(200).json(tagData)
+    } catch (err) {
+      res.status(500).json(err);
+    }
+
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  const newTag = req.body;
+  try {
+    const tagData = await Tag.create(newTag);
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
