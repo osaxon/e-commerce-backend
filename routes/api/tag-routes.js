bd73de1560ff56409189fb27e70a8b56f47f2a20
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-21 18:15:15 +0100

    FEAT: Get all tag routes added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,16 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag,
+        attributes: ['id', 'product_name', 'price', 'stock'],
+      }]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
